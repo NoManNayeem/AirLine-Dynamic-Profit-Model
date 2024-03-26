78881730053b5631b30abce62dc6aeac42f3a47a
NoManNayeem <nayeem60151126@gmail.com>
2024-03-27 00:00:21 +0600

    first commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 # Load the model (adjust the path as necessary)
 model_path = 'C:/Users/Nayeem Islam/Desktop/AirLine Dynamic Profit - ML/notebooks/model/ticket_price_predictor_rf.joblib'
 model = load(model_path)
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,8 +18,14 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    input_df = pd.DataFrame([data])
+
+    # Construct a DataFrame ensuring the columns order matches the training data's
+    input_df = pd.DataFrame([data], columns=['Day of Week', 'Time of Day', 'Origin', 'Destination', 
+                                             'Duration', 'Seats Available', 'Historical Load Factor', 'Competitor Price'])
+    
+    # Preprocessing steps (if any) should be part of the pipeline loaded with the model
     prediction = model.predict(input_df)[0]
+    
     return jsonify({'predicted_price': prediction})
 
 if __name__ == '__main__':
